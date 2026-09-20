bb3b98c16952d55034fea9befd1bc90e66f4e156
azlouk96 <ahmedazlouk9@gmail.com>
2026-09-20 13:15:05 +0100

    feat (reports): skills

--- src/components/section/hackathons-section.tsx
+++ src/components/section/hackathons-section.tsx
@@ -1,4 +1,4 @@
-//@ts-nocheck
+
 /* eslint-disable @next/next/no-img-element */
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
@@ -52,23 +52,7 @@ export default function HackathonsSection() {
                     {hackathon.description}
                   </p>
                 )}
-                {hackathon.links && hackathon.links.length > 0 && (
-                  <div className="mt-1 flex flex-row flex-wrap items-start gap-2">
-                    {hackathon.links.map((link, idx) => (
-                      <Link
-                        href={link.href}
-                        key={idx}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <Badge className="flex items-center gap-1.5 text-xs bg-primary text-primary-foreground">
-                          {link.icon}
-                          {link.title}
-                        </Badge>
-                      </Link>
-                    ))}
-                  </div>
-                )}
+            
               </div>
             </TimelineItem>
           ))}
